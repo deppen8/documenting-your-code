a6b04fe5f3c5d51ff72d86e96574624bd2c08a7c
Jacob Deppen <jacobdeppen@granular.ag>
2022-04-19 09:31:17 -0400

    add comments, docstrings, and type hints

--- randomly/facts.py
+++ randomly/facts.py
@@ -1,19 +1,49 @@
+from typing import Dict, Union
+
 import requests
 from requests.exceptions import RequestException
 
 
-def generate_random_fact(output_format, language):
+def generate_random_fact(output_format: str, language: str) -> Union[str, Dict]:
+    """Fetch a random fact from the Useless Facts API
+
+    Parameters
+    ----------
+    output_format : {"html", "json", "txt", "md"}
+        Desired format for the returned fact.
+    language : {"en", "de"}
+        Desired language for the fact. English and German supported.
+
+    Returns
+    -------
+    fact : Union[str, Dict]
+        The result of the query. If ``output_format=="json"``, it returns a dictionary.
+        Otherwise, a string is returned.
+
+    Raises
+    ------
+    ValueError
+        When ``language`` is not one of ``{"en", "de"}``.
+    ValueError
+        When ``output_format`` not one of ``{"html", "json", "txt", "md"}``.
+    RequestException
+        When the request response is anything other than a 200 (success).
+    """
+
+    # Validate that inputs match supported values
     if language not in {"en", "de"}:
         raise ValueError(f"{language} is not supported.")
 
     if output_format not in {"html", "json", "txt", "md"}:
         raise ValueError(f"{output_format} is not supported.")
 
+    # Query random fact API
     response = requests.get(
         f"https://uselessfacts.jsph.pl/random.{output_format}?language={language}"
     )
 
     if response.status_code == 200:
+        # Get the proper format for the output
         if output_format == "json":
             fact = response.json()
         else:
--- randomly/password.py
+++ randomly/password.py
@@ -1,16 +1,41 @@
 import random
 import string
+from typing import Iterable, Optional
 
 
-def generate_password(chars, punctuation, invalid_chars=None):
+def generate_password(
+    chars: int, punctuation: bool, invalid_chars: Optional[Iterable] = None
+) -> str:
+    """Make a random string of any length.
+
+    Parameters
+    ----------
+    chars : int
+        Number of characters desired in the output.
+    punctuation : bool
+        Whether or not to include punctuation characters.
+    invalid_chars : Optional[Iterable], optional
+        An iterable of characters that should not appear in the result. By default ``None``,
+        in which case no extra characters are excluded.
+
+    Returns
+    -------
+    str
+        A random string that can be used as a password.
+    """
+    # Define character set to use
     valid_chars = string.ascii_letters + string.digits
 
+    # Add punctuation, if desired
     if punctuation:
         valid_chars += string.punctuation
 
-    for invalid_char in invalid_chars:
-        valid_chars = valid_chars.replace(invalid_char, "")
+    # If invalid characters are specified, remove those characters
+    if invalid_chars is not None:
+        for invalid_char in invalid_chars:
+            valid_chars = valid_chars.replace(invalid_char, "")
 
+    # Randomly choose from the valid character set and turn into a single str
     password_chars = random.choices(valid_chars, k=chars)
     password = "".join(char for char in password_chars)
 
